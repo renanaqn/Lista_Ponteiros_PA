11c8dea1211b5f6a6b7e35b0e232f011541cde55
Renan de Aquino <renanap99@gmail.com>
2021-11-16 15:50:31 -0300

    Create Questao09.c

---
+++ Questao09.c
@@ -0,0 +1,18 @@
+/*
+X sendo declarado como char:
+X+1 = 4093
+X+2 = 4094
+X+3 = 4095
+X sendo declarado como int:
+X+1 = 4094
+X+2 = 4096
+X+3 = 4098
+X sendo declarado como float:
+X+1 = 4096
+X+2 = 4100
+X+3 = 4104
+X sendo declarado como double:
+X+1 = 4100
+X+2 = 4108
+X+3 = 4116
+*/
