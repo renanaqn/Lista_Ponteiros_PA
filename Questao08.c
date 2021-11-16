27bceb6014c39f1bde6fc2d8a098b45af22189e0
Renan de Aquino <renanap99@gmail.com>
2021-11-16 15:49:39 -0300

    Create Questao08.c

---
+++ Questao08.c
@@ -0,0 +1,18 @@
+/*int main(){
+  int vet[] = {4,9,13};
+  int i;
+  for(i=0;i<3;i++){
+  printf("%d ",*(vet+i));
+  }
+}*/ 
+// Esse primeiro Algoritmo mostra o conteúdo dos índices do vetor usando o asterisco
+
+/*int main(){
+  int vet[] = {4,9,13};
+  int i;
+  for(i=0;i<3;i++){
+  printf("%X ",vet+i);
+  }
+}*/
+
+//Já esse outro algoritmo mostra o endereço dos índices do vetor
