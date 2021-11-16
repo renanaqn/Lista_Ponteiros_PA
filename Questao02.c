2b4a02c862126e6842718a4e5fa06495ee2a8f8d
Renan de Aquino <renanap99@gmail.com>
2021-11-16 15:13:53 -0300

    Update Questao02.c

--- Questao02.c
+++ Questao02.c
@@ -5,5 +5,5 @@ int i=5, *p;
 p = &i;
 printf("%x %d %d %d %d \n", p,*p+2,**&p,3 **p,**&p+4);
 // na questão em específico retorna: 4004 7 15 5 9
-// na execução real retorna: FFE 7 15 5 9
+// na execução no QT retorna: 0061FE1C 7 15 5 9
 }
